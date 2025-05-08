41141f68218f4ccfe8c6a1e6f2977470ecb9a857
GuoZimo66 <3159168483@qq.com>
2025-05-08 19:12:02 +0800

    update2

---
+++ src/TupleSpaceClient.java
@@ -0,0 +1,2 @@
+public class TupleSpaceClient {
+}
---
+++ src/TupleSpaceServer.java
@@ -0,0 +1,26 @@
+
+public class TupleSpaceServer {
+    private static final int UPDATE_INTERVAL = 10000; // 10 秒
+
+    public TupleSpaceServer(int port) {
+
+    }
+    public static void main(String[] args) {
+    }
+        private void startSummaryPrinter() {
+
+        }
+
+
+    private void printSummary() {
+
+    }
+
+
+    public void start() {
+
+    }
+    private class ClientHandler extends Thread {
+
+    }
+}
